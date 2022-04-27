ae9946a8e90b94bc8e906749f691a44e61bba134
Jason Liu <jasonliu00@gmail.com>
2022-04-26 22:17:40 -0400

    Initial Commit

---
+++ Chinese Reader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chinese Reader
+//
+//  Created by Jason Liu on 4/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
